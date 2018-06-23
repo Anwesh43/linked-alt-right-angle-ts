0a23c28f684c130719a512eae75cfbbae9bb90d3
Anwesh43 <anweshthecool0@gmail.com>
2018-06-23 21:59:39 +0530

    adding ARANode class

--- LinkedAltRightAngleStage.ts
+++ LinkedAltRightAngleStage.ts
@@ -80,3 +80,63 @@ class ARAAnimator {
         }
     }
 }
+
+class ARANode {
+
+    state : ARAState = new ARAState()
+
+    next : ARANode
+
+    prev : ARANode
+
+    constructor(private i : Int) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < ARA_NODES - 1) {
+            next = ARANode(this.i + 1)
+            next.prev = this
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        const gap : number = (0.9 * Math.min(w, h)) / ARA_NODES
+        context.save()
+        context.translate(gap/10 + (this.i * gap) + gap * this.state.scales[0], h - gap /10 - (this.i * gap))
+        var index : number = this.i%2
+        var scale = index * (1 - this.state.scales[1]) + (this.state.scales[1]) * (1 - index)
+        context.lineCap = 'round'
+        context.lineWidth = Math.min(w, h) / 60
+        context.strokeStyle = '#9b59b6'
+        for(var i = 0; i < 2; i++) {
+            context.save()
+            context.rotate(scale * i * -Math.PI/2)
+            context.moveTo(0, 0)
+            context.lineTo(0, gap)
+            context.stroke()
+            context.restore()
+        }
+        context.restore()
+    }
+
+    update(stopcb : Function) {
+        this.state.update(stopcb)
+    }
+
+    startUpdating(startcb : Function) {
+        this.state.startUpdating(startcb)
+    }
+
+    getNext(dir : number, cb : Function) {
+        var curr : ARANode = this.prev
+        if (dir == 1) {
+            curr = this.next
+        }
+        if (curr) {
+            return curr
+        }
+        cb()
+        return this
+    }
+}
